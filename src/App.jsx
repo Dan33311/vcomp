c8eda4bbc147145a41c6d747122dc7cee8a8da3c
dan33311 <dansaro.groger@gmail.com>
2023-09-19 17:11:04 -0500

    create a Camera component, use a webcam package

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
+import React from 'react'
 import './App.css'
-import React, { useRef } from 'react';
+import Camera from './components/Camera'
+
 
 function App() {
   return (
     <>
-      <h1>ViCOMP</h1>
-
-      <p>
-        Edit <code>src/App.jsx</code> and save to test HMR
-      </p>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className="title">ViCOMP</h1>
+      <div className="hero">
+        <Camera />
+      </div>
     </>
-  );
+  )
 }
 
 export default App
