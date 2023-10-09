bb7a09a53e9aee0cd130fa747f3263e3d0023e12
dan33311 <dansaro.groger@gmail.com>
2023-10-09 12:47:56 -0500

    use dotenv with vite in my firebase configuration

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,14 @@ import { getStorage } from 'firebase/storage'
 
 
 const firebaseConfig = {
-  // apiKey: import.meta.env.apiKey,
-  // authDomain: import.meta.env.authDomain,
-  // projectId: import.meta.env.projectId,
-  // storageBucket: import.meta.env.storageBucket,
-  // messagingSenderId: import.meta.env.messagingSenderId,
-  // appId: import.meta.env.appId,
-  // measurementId: import.meta.env.measurementId,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SNDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 }
 
 const app = initializeApp(firebaseConfig)
